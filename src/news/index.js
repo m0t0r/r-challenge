f4bcb25047df79f80052f094b78d1d406acf7a91
m0t0r <tkachenko.vitaly.job@gmail.com>
2016-07-02 14:27:06 -0500

    feat(components): Create r-news-panel component

--- src/news/index.js
+++ src/news/index.js
@@ -1,5 +1,9 @@
 import angular from 'angular';
 
-const newsModule = angular.module('rApp.news',[]);
+import rNewsPanelComponent from './r-news-panel/r-news-panel.component';
+
+const newsModule = angular
+  .module('rApp.news', [])
+  .component('rNewsPanel', rNewsPanelComponent);
 
 export default newsModule.name;
---
+++ src/news/r-news-panel/r-news-panel.component.js
@@ -0,0 +1,12 @@
+import template from './r-news-panel.html';
+import './r-news-panel.css';
+
+class rNewsPanelCtrl { }
+
+let rNewsPanelComponent = {
+  transclude: true,
+  controller: rNewsPanelCtrl,
+  template
+};
+
+export default rNewsPanelComponent;
