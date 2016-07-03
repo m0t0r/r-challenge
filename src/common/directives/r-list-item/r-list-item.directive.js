2036114f4c02219beac2960251127f56ff9023b1
m0t0r <tkachenko.vitaly.job@gmail.com>
2016-07-02 19:38:32 -0500

    feat(directives): Make r-list and r-list-item work together and add more interactions

--- src/common/directives/r-list-item/r-list-item.directive.js
+++ src/common/directives/r-list-item/r-list-item.directive.js
@@ -1,16 +1,26 @@
 import template from './r-list-item.html';
 import './r-list-item.css';
 
-class rListItemCtrl { }
+function rListItemLinkFn(scope, elm, attrs, rListCtrl) {
+  'use strict';
+
+  elm.find('r-list-item-preview').on('click', () => {
+    rListCtrl.selectItem(elm, scope.itemData);
+  });
+  
+}
 
 let rListItemDirective = () => {
   return {
-    scope: {},
+    require: '^rList',
+    scope: {
+      itemData: '='
+    },
     transclude: {
       previewSlot: '?rListItemPreview',
       fullSlot: '?rListItemFull'
     },
-    controller: rListItemCtrl,
+    link: rListItemLinkFn,
     template
   }
 };
--- src/common/directives/r-list/r-list.directive.js
+++ src/common/directives/r-list/r-list.directive.js
@@ -1,11 +1,43 @@
 import template from './r-list.html';
 import './r-list.css';
 
-class rListCtrl { }
+class rListCtrl { 
+
+  constructor() {
+    this.collapsed = true;
+  }
+
+  selectItem(selectedItem, itemData) {
+    this.collapsed = !this.collapsed;
+    this.rListItemFullElm = selectedItem.find('r-list-item-full')[0];
+    var newHeight = this.collapsed ? 0 : this._getElementAutoHeight();
+
+    this.rListItemFullElm.style.height = newHeight + 'px';
+    
+    this.onSelectedItem({item: itemData});
+  }
+
+  _getElementAutoHeight() {
+    var currentHeight = this._getElementCurrentHeight();
+    this.rListItemFullElm.style.height = 'auto';
+    var autoHeight = this._getElementCurrentHeight();
+
+    this.rListItemFullElm.style.height = currentHeight;
+    this._getElementCurrentHeight(); // Force the browser to recalc height after moving it back to normal
+  
+    return autoHeight;
+  }
+
+  _getElementCurrentHeight() { return this.rListItemFullElm.offsetHeight; }
+}
 
 let rListDirective = () => {
   return {
-    scope: {},
+    scope: {
+      onSelectedItem: '&'
+    },
+    bindToController: true,
+    controllerAs: "$ctrl",
     transclude: true,
     controller: rListCtrl,
     template
