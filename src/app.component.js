63722eca87fd059a36343063fb1e8b4c68748756
m0t0r <tkachenko.vitaly.job@gmail.com>
2016-07-05 22:04:26 -0500

    feat(app): Add more responsivness

--- src/app.component.js
+++ src/app.component.js
@@ -1,22 +1,6 @@
 import template from './app.component.html';
 
 class AppCtrl {
-  
-  /*constructor(NewsService, $scope) {
-    this.newsService = NewsService;
-    this.$scope = $scope;
-  }
-  
-  loadNews() {
-    this.newsService.getNews().then((response) => {
-      this.news = response.data;
-    })
-  }
-
-  setSelectedItem(item){
-    this.selectedItem = item;
-    this.$scope.$apply();
-  }*/
 
   constructor($state) {
     this.$state = $state;
